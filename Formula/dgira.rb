073dd2befda6431effa352c481e6e0e06ba8a5d9
Oli Dacombe <oliver.dacombe@gsr.io>
2024-07-01 12:25:50 +0100

    upgrade dgira

--- Formula/dgira.rb
+++ Formula/dgira.rb
@@ -1,9 +1,9 @@
 class Dgira < Formula
   desc "Jira cli tool for use in productivity tooling"
   homepage "https://github.com/olidacombe/dgira"
-  url "https://github.com/olidacombe/dgira/releases/download/0.1.4/dgira-0.1.4-x86_64-apple-darwin.tar.gz"
-  sha256 "1b2ca3e87ca840ec87ba57e6d5c3e503456e07e484720b089513c28b49678c0c"
-  version "0.1.4"
+  url "https://github.com/olidacombe/dgira/releases/download/0.1.5/dgira-0.1.5-x86_64-apple-darwin.tar.gz"
+  sha256 "89c8b09a24de765dbc6b2a21038265dba9997a22af8ea65145092fe1f2b64176"
+  version "0.1.5"
 
   def install
     bin.install "dgira"
